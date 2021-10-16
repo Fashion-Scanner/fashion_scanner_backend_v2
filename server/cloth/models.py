cfef1ffb3fc430e887f3a6b254e2aff79fe923c2
bbFactory <borahm87@gmail.com>
2021-10-16 06:53:18 +0000

    Feat : category 모델(cloth) 추가

--- server/cloth/models.py
+++ server/cloth/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from server.base.models import BaseModel
 
-# Create your models here.
+class Category(models.Model):
+    ko_name = models.CharField(max_length=32, verbose_name="의류 카테고리 한국어")
+    en_name = models.CharField(max_length=32, verbose_name="의류 카테고리 영어")
+
+    def __str__(self):
+        return f"{self.en_name}({self.ko_name})"
+
+    class Meta:
+        db_table = "cloth_category"
+        verbose_name = "의류 카테고리"
+        verbose_name_plural = "의류 카테고리"
+        ordering = ["id"]
+
