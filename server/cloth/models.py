d0f6f0bc04a1186a465a01937c54fa6ecedfb0f1
bbFactory <borahm87@gmail.com>
2021-10-16 07:14:00 +0000

    Feat : clothes 모델(cloth) 추가

--- server/cloth/models.py
+++ server/cloth/models.py
@@ -1,6 +1,51 @@
 from django.db import models
 from server.base.models import BaseModel
 
+
+def image_upload_to(instance, filename):
+    return os.path.join(instance.UPLOAD_PATH, f"{filename}")
+
+
+class Clothes(BaseModel):
+    UPLOAD_PATH = "clothes"
+
+    image = models.ImageField(
+        upload_to=image_upload_to,
+        verbose_name="의류 이미지",
+    )
+    member = models.ForeignKey(
+        "member.Members",
+        null=True,
+        verbose_name="멤버",
+        on_delete=models.CASCADE
+    )
+    color = models.ForeignKey(
+        "common.Color",
+        null=True,
+        verbose_name="의류 색상",
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        "cloth.Category",
+        null=True,
+        verbose_name="의류 카테고리",
+        on_delete=models.CASCADE
+    )
+    attributes = models.ManyToManyField(
+        "cloth.Attribute",
+        verbose_name="의류 속성",
+    )
+
+    def __str__(self):
+        return f"의류({self.id})"
+
+    class Meta:
+        db_table = "cloth_clothes"
+        verbose_name = "의류"
+        verbose_name_plural = "의류"
+        ordering = ["id"]
+
+
 class Category(models.Model):
     ko_name = models.CharField(max_length=32, verbose_name="의류 카테고리 한국어")
     en_name = models.CharField(max_length=32, verbose_name="의류 카테고리 영어")
