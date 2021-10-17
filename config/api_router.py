ac54471caa2a8c6c60b0bfdb99d792f8a1b98f63
HyunAm <tlfgjawlq@naver.com>
2021-10-17 14:52:58 +0900

    Feat : cloth api 추가
    - cloth api 추가(테스트 필요)

--- config/api_router.py
+++ config/api_router.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.urls import include, path
+from server.cloth.api.views import ClothViewSet
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
+
+router.register("cloth", ClothViewSet, basename="cloth")
 
 app_name = "api"
 urlpatterns = router.urls
