59b256e0d7f4e441966162008178539169e80115
HyunAm <tlfgjawlq@naver.com>
2021-10-17 15:31:13 +0900

    Feat : Member api 추가

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.urls import include, path
+
 from server.cloth.api.views import ClothViewSet
+from server.member.api.views import MemberViewset
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 if settings.DEBUG:
@@ -9,6 +11,7 @@ else:
     router = SimpleRouter()
 
 router.register("cloth", ClothViewSet, basename="cloth")
+router.register("member", MemberViewset, basename="member")
 
 app_name = "api"
 urlpatterns = router.urls
